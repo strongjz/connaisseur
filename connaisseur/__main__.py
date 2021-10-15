7e4a0f721ede150f143d444813d5a3bcd9273838
Teetje Stark <teetje.stark@gmx.de>
2021-10-15 12:07:33 +0200

    Debug

--- connaisseur/__main__.py
+++ connaisseur/__main__.py
@@ -40,6 +40,7 @@ if __name__ == "__main__":
         '/health': healthz,
         '/ready': readyz,
     }
+    log = logging.getLogger('wsgidav')
     paths = PathInfoDispatcher(path_map)
     server = Server(('0.0.0.0', 5000), APP)
     server.start()
