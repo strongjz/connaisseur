a35107c1bb148016192e9530362fb38041a85915
Teetje Stark <teetje.stark@gmx.de>
2021-10-15 12:22:22 +0200

    Pylint

--- connaisseur/__main__.py
+++ connaisseur/__main__.py
@@ -2,11 +2,13 @@
 Main method for connaisseur. It starts the web server.
 """
 import os
+
 from logging.config import dictConfig
-from connaisseur.flask_server import *
 from cheroot.server import HTTPServer
 from cheroot.ssl.builtin import BuiltinSSLAdapter
 from cheroot.wsgi import Server, PathInfoDispatcher
+
+from connaisseur.flask_server import *
 
 if __name__ == "__main__":
     LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
